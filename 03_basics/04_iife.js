83d7b2d56fb2d24b7453c2cf2f5dbdc2e7069a03
Madhavi Joshi <109155050+Madhavi210@users.noreply.github.com>
2024-03-29 15:52:23 +0530

    updated

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -18,7 +18,9 @@
 /*
 Use cases
 Avoid polluting the global namespace
-Because our application could include many functions and global variables from different source files, it's important to limit the number of global variables. If we have some initiation code that we don't need to use again, we could use the IIFE pattern. As we will not reuse the code again, using IIFE in this case is better than using a function declaration or a function expression.
+Because our application could include many functions and global variables from different source files, it's important to limit the number of global variables. 
+If we have some initiation code that we don't need to use again, we could use the IIFE pattern. As we will not reuse the code again, 
+using IIFE in this case is better than using a function declaration or a function expression.
  */
 
 
